87636bccff5df64efb1662ba92d3e73856638d4f
muneeba <muneebawaseem1006@gmail.com>
2024-06-01 14:09:58 +0500

    Conditions committed

---
+++ Conditional_Statement/conditions.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a = 22;
+    if (a < 18)
+    {
+        cout << "You are undergraduate." << endl;
+    }
+    else
+        {
+            cout << "You are graduated.";
+        }
+        return 0;
+}
