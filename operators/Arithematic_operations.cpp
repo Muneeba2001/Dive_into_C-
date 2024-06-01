e1fc8b6ec4a5f347418e9b337b25ce9d06da9af7
muneeba <muneebawaseem1006@gmail.com>
2024-06-01 14:03:01 +0500

    Cpp committed

---
+++ operators/Arithematic_operations.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a = 10; // Datatype integer: which only takes number!
+    int b = 20;
+    // Addition 
+    cout<< "The addition of the a and b = "<< a+b<<endl;
+    // Substraction 
+    cout<< "The substraction of b and a = "<< b-a<<endl;
+    // Multiplication 
+    cout<< "The multiplication of a and b = "<< a*b<<endl;
+    // Division 
+    cout<< "The Division of both a and b = "<< b/a<<endl;
+}
