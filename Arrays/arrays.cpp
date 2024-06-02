d89dd6dd138428fb149a0167e85cbea5a119d0fb
muneeba <muneebawaseem1006@gmail.com>
2024-06-02 10:16:55 +0500

    ArraythroughLoop

---
+++ Arrays/arrays.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    string arr[]={"abc", "efg", "hij", "klm", "nop"};
+    for(int i=0; i<5; i++){
+        cout<<arr[i]<<endl;
+    }
+    return 0;
+}
